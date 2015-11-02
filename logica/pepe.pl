04bbad9cc5050697661fca4095a071e969f54dd3
chudi <elchudi@gmail.com>
2015-11-01 21:07:08 -0300

    irirate hiciste algo alguna vez!

--- logica/pepe.pl
+++ logica/pepe.pl
@@ -29,4 +29,36 @@ resumenPiezas(L, [X|Tail]) :-  X = pieza(T,N), cantidadDeApariciones(T,L,N), sub
 %  cuyos valores suman Total. Aquí no se pide controlar que la cantidad de cada pieza
 %  esté acorde con la disponibilidad.
 
-% generar(Total,Piezas,Sol):- member(X,Sol), X = pieza(T,N), member(Y,Piezas), Y = pieza(T2,N2), T = T2, N =< N2, suma(SOL) = Total
+suma([], 0).
+suma([X|T], Sum) :- X = pieza(Tam,Cant), suma(T,Sum2), Sum is Sum2 + Tam * Cant.  
+
+%generar(Total,Piezas,Sol):-  suma(Sol, Sum), Sum == Total, member(X,Sol), member(Y,Piezas), X = pieza(T1,_), Y = pieza(T2,_), T1 is T2.
+
+
+%generar(0,_,[]).
+%generar(Total,Piezas,Sol):- listaNats()
+
+
+generar2(Total,Piezas,Sol):- length(Sol,N), N =< Total, 
+							member(X,Sol), member(Y,Piezas), 
+							between(0,Total, T1), between(0,Total, N1), X = pieza(T1,N1), 
+							Y = pieza(T2,_), T1 == T2,
+							suma(Sol, Sum), Sum == Total.
+
+
+%%% Ejercicio 5 
+
+% cumpleLímite(+Piezas,+Solución) será verdadero cuando la cantidad de piezas utilizadas en Solución 
+%  no exceda las cantidades disponibles indicadas en Piezas
+
+cumpleLimite(_,[]).
+cumpleLimite(Piezas,[pieza(T,N)| Tail]):- member(X,Piezas), X = pieza(T1,N1), T1 == T, N =< N1, cumpleLimite(Piezas, Tail).
+
+
+%%% Ejercicio 6
+
+% construir1(+Total,+Piezas,-Solución), donde Solución representa una lista de piezas cuyos valores 
+%  suman Total y, además, las cantidades utilizadas de cada pieza no exceden los declarados en Piezas.
+
+construir1(Total,Piezas,Solucion):- generar2(Total,Piezas,Solucion), cumpleLimite(Piezas, Solucion)	.
+
